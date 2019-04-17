11c527a50d0e3e21ce1e6ee477a158b64fd52c20
rfi <rfi@audaxis.com>
2019-04-17 13:26:41 +0200

    Add chart from history data

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type (
@@ -31,16 +31,18 @@ type (
 	HTMLPage struct {
 		PageTitle string
 		History   []History
+		LastDate  string
 	}
 )
 
 var (
+	//CDPData for page history
 	CDPData []History
-	//TemplateFile *string
 )
 
 func main() {
 	jsonFile := flag.String("file", "", "History file from your cdp.")
+	port := flag.Int("port", 80, "Port to use for the service")
 	flag.Parse()
 
 	if "" == *jsonFile {
@@ -58,56 +60,23 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//fmt.Println(CDPData)
-
-	//for _, v := range CDPData {
-	//	fmt.Println(v.CDP)
-	//}
-
-	//for _, v := range vJSON {
-	//	fmt.Printf("%+v \n", v.Date)
-	//	fmt.Printf("%+v \n", v.CDP)
-	//}
-
-	//tmpl, err := template.ParseFiles("templates/template.html")
-
 	http.HandleFunc("/", index)           // set router
-	err = http.ListenAndServe(":80", nil) // set listen port
+	err = http.ListenAndServe(":"+ strconv.Itoa(*port), nil) // set listen port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
-
-	//fmt.Println("The next one ought to fail.")
-	//tErr := template.New("check parse error with Must")
-	//template.Must(tErr.Parse(" some static text {{ .Name }"))
-
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-
 	tmpl := template.Must(template.ParseFiles("templates/template.html"))
-	//datas := make([]CDP, len(*CDPData))
-	//for _, v := range CDPData {
-	//	r := v.CDP
-	//	//fmt.Println("test", r)
-	//	//datas = append(datas, *r)
-	//}
-	//fmt.Println(CDPData)
-	//fmt.Println(datas)
+	lastDate := CDPData[len(CDPData)-1].Date
 	data := HTMLPage{
-		PageTitle: "Mon super site internet",
+		PageTitle: "CDP - HISTORY",
 		History:   CDPData,
+		LastDate:  lastDate,
 	}
-	tmpl.Execute(w, data)
-
-	//r.ParseForm()  // parse arguments, you have to call this by yourself
-	//fmt.Println(r.Form)  // print form information in server side
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	//fmt.Println(r.Form["url_long"])
-	//for k, v := range r.Form {
-	//    fmt.Println("key:", k)
-	//    fmt.Println("val:", strings.Join(v, ""))
-	//}
-	//fmt.Fprintf(w, data) // send data to client side
+	err := tmpl.Execute(w, data)
+	if nil != err {
+		log.Println(err)
+	}
 }
